39fd681c2232ca276f9c5079690cd97f1ae9c277
Jay <johnsonjohnadakole@gmail.com>
2023-07-14 23:07:12 +0100

    init: initialised the pool page

---
+++ src/app/pool/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const pool = () => {
+    return (
+        <>
+            <div>
+                <p>pool</p>
+            </div>
+        </>
+    )
+};
+
+export default pool
