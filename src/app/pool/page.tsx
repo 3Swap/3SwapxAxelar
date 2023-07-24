40f33d343f34c31c2430b840b23f7f0b4c3353a1
Jay <johnsonjohnadakole@gmail.com>
2023-07-24 09:17:23 +0100

    initial

--- src/app/pool/page.tsx
+++ src/app/pool/page.tsx
@@ -3,9 +3,17 @@ import React from 'react'
 const pool = () => {
     return (
         <>
-            <div>
-                <p>pool</p>
+            <div className="min-h-screen pt-12 mx-16">
+                <div className="space-y-6">
+                    <p className="text-5xl font-bold w-full">
+                        Connect Your Wallet to see assets available to stake</p>
+                    <p>
+                        Add liquidity to stargate's omnichain protocol and earn stablecoin rewards on every Stargate transfer.
+                        Liquidity Providers can also farm their Lp tokens to receive STG token rewards.
+                    </p>
+                </div>
             </div>
+
         </>
     )
 };
